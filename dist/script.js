7a9068e61484aa4e08eb8e0621eeb6254006835a
enl001 <enliven001@mail.ru>
2020-03-26 12:13:15 +0300

    feat: add webpack config

--- dist/script.js
+++ dist/script.js
@@ -93,8 +93,188 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("\nconst Keyboard = {\n  elements: {\n  main: null,\n  keyboardContainer:null,\n  keys:[]\n  },\n  eventHandlers: {\n    onInput : null,\n    onClose : null\n  },\n  properties: {\n    value: '',\n    capsLock: false\n  },\n\n  init() {\n    // create keyboard\n    console.log('init');\n    this.elements.main = document.createElement('div');\n    this.elements.keyboardContainer = document.createElement('div');\n\n    // setup keyboard elements\n    this.elements.main.classList.add('keyboard-pannel','1keyboard-pannel_hidden');\n    this.elements.keyboardContainer.classList.add('keyboard-pannel__keys');\n\n    // add to DOM\n    this.elements.main.appendChild(this.elements.keyboardContainer);\n    document.querySelector('.keyboard > .wrapper').appendChild(this.elements.main);\n\n  \n  },\n  _createKeys() {\n    const fragment = document.createDocumentFragment();\n    const keyLayout = ['1','2','3','4','5','6','7','8','9','0','backspace',\n          'q','w','e','r','t','y','u','i','o','p',\n          'caps','a','s','d','f','g','h','j','k','l','enter',\n          'shift','z','x','c','v','b','n','m',',','.','?',\n          'ctrl','alt','space','alt','ctrl'];\n\n    // create HTML for an icon\n    const createIconHTML = (iconName) => {\n      return `<i class='material-icons'>${iconName}</i>`;\n    };\n\n    keyLayout.forEach(key => {\n      const keyElement = document.createElement('div');\n      const keyContent = document.createElement('span');\n      const insertLineBreak = ['backspace', 'p', 'enter','?'].indexOf(key) !== -1;\n\n      //add classes\n      keyElement.classList.add('key');\n\n      switch (key){\n        case 'backspace':\n          keyElement.classList.add('key_medium');\n          keyContent.innerHTML = createIconHTML('backspace');\n          keyElement.appendChild(keyContent);\n          keyElement.addEventListener('click', () => {\n            this.properties.value = this.properties.value\n            .substring(0, this.properties.value.length-1);\n            this._triggerEvent('onInput');\n          });\n\n          break;\n\n        case 'caps':\n          keyElement.classList.add('key_medium', 'key_activatable');\n          keyContent.innerHTML = createIconHTML('keyboard_capslock');\n          keyElement.appendChild(keyContent);\n          keyElement.addEventListener('click', () => {\n            this._toggleCapsLock();  \n            keyElement.classList.toggle('key_active',this.properties.capsLock);          \n          });\n\n          break;  \n\n      }\n\n\n    });\n\n  },\n  _triggerEvent(handlerName){\n    console.log('Event Triggered! Event name: '+handlerName);\n  },\n  _toggleCapsLock() {\n        console.log('CapsLock toggled');\n\n  },\n  open(initialValue, onInput, onClose) {\n\n  },\n  close() {\n\n  }\n\n};\n\n\nwindow.addEventListener('DOMContentLoaded', function() {\n  createDomTree();\n  Keyboard.init();\n  \n});\n\n// window.onload = () => {\n//   console.log('onload');\n  \n// };\n\n\n\n\nfunction createSection(className){\n  const section = document.createElement('section');\n  const div = document.createElement('div');\n  div.classList.add('wrapper');\n  if(className) section.classList.add(className);\n  section.append(div);\n  return section;\n}\n\nfunction createButton(text, classList){  \n  const key = document.createElement('div');\n  const symbol = document.createElement('span');\n  symbol.innerText = text;  \n  key.classList.add(...classList);\n  key.append(symbol); \n  return key;\n}\n\nfunction createKeyboard(){\n  const keyboardPannel = document.createElement('div');\n  keyboardPannel.classList.add('keyboard-pannel');\n  const div = document.createElement('div');\n  div.classList.add('keyboard-pannel__keys');\n\n  let key = createButton('h', ['key']);\n  div.append(key);\n  key = createButton('Alt', ['key','key_medium']);\n  div.append(key);\n  key = createButton('Shift', ['key','key_wide']);\n  div.append(key);\n  key = createButton(' ', ['key','key_extra-wide']);\n  div.append(key);\n  key = createButton('Caps Lock', ['key','key_medium', 'key_activatable', 'key_active']);\n  div.append(key);\n\n  keyboardPannel.append(div);\n  return keyboardPannel;\n}\n\nconst createDomTree = () => {\n  console.log('domTree');\n  const header = document.createElement('header');\n  const main = document.createElement('main');\n  const footer = document.createElement('footer');\n  const div = document.createElement('div');\n  div.classList.add('wrapper');\n  header.classList.add('header');\n  header.append(div);\n  main.classList.add('main');  \n  footer.classList.add('footer');\n  footer.append(div);\n\n  const screen = createSection('screen');\n  const keyboard = createSection('keyboard');\n  // keyboard.querySelector('.wrapper').append(createKeyboard());\n\n  main.append(screen);\n  main.append(keyboard);\n\n  document.body.append(header);\n  document.body.append(main);\n  document.body.append(footer);\n};\n\n//https://www.youtube.com/watch?v=N3cq0BHDMOY\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+const Keyboard = {
+  elements: {
+  main: null,
+  keyboardContainer:null,
+  keys:[]
+  },
+  eventHandlers: {
+    onInput : null,
+    onClose : null
+  },
+  properties: {
+    value: '',
+    capsLock: false
+  },
+
+  init() {
+    // create keyboard
+    console.log('init');
+    this.elements.main = document.createElement('div');
+    this.elements.keyboardContainer = document.createElement('div');
+
+    // setup keyboard elements
+    this.elements.main.classList.add('keyboard-pannel','1keyboard-pannel_hidden');
+    this.elements.keyboardContainer.classList.add('keyboard-pannel__keys');
+
+    // add to DOM
+    this.elements.main.appendChild(this.elements.keyboardContainer);
+    document.querySelector('.keyboard > .wrapper').appendChild(this.elements.main);
+
+  
+  },
+  _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = ['1','2','3','4','5','6','7','8','9','0','backspace',
+          'q','w','e','r','t','y','u','i','o','p',
+          'caps','a','s','d','f','g','h','j','k','l','enter',
+          'shift','z','x','c','v','b','n','m',',','.','?',
+          'ctrl','alt','space','alt','ctrl'];
+
+    // create HTML for an icon
+    const createIconHTML = (iconName) => {
+      return `<i class='material-icons'>${iconName}</i>`;
+    };
+
+    keyLayout.forEach(key => {
+      const keyElement = document.createElement('div');
+      const keyContent = document.createElement('span');
+      const insertLineBreak = ['backspace', 'p', 'enter','?'].indexOf(key) !== -1;
+
+      //add classes
+      keyElement.classList.add('key');
+
+      switch (key){
+        case 'backspace':
+          keyElement.classList.add('key_medium');
+          keyContent.innerHTML = createIconHTML('backspace');
+          keyElement.appendChild(keyContent);
+          keyElement.addEventListener('click', () => {
+            this.properties.value = this.properties.value
+            .substring(0, this.properties.value.length-1);
+            this._triggerEvent('onInput');
+          });
+
+          break;
+
+        case 'caps':
+          keyElement.classList.add('key_medium', 'key_activatable');
+          keyContent.innerHTML = createIconHTML('keyboard_capslock');
+          keyElement.appendChild(keyContent);
+          keyElement.addEventListener('click', () => {
+            this._toggleCapsLock();  
+            keyElement.classList.toggle('key_active',this.properties.capsLock);          
+          });
+
+          break;  
+
+      }
+
+
+    });
+
+  },
+  _triggerEvent(handlerName){
+    console.log('Event Triggered! Event name: '+handlerName);
+  },
+  _toggleCapsLock() {
+        console.log('CapsLock toggled');
+
+  },
+  open(initialValue, onInput, onClose) {
+
+  },
+  close() {
+
+  }
+
+};
+
+
+window.addEventListener('DOMContentLoaded', function() {
+  createDomTree();
+  Keyboard.init();
+  
+});
+
+// window.onload = () => {
+//   console.log('onload');
+  
+// };
+
+
+
+
+function createSection(className){
+  const section = document.createElement('section');
+  const div = document.createElement('div');
+  div.classList.add('wrapper');
+  if(className) section.classList.add(className);
+  section.append(div);
+  return section;
+}
+
+function createButton(text, classList){  
+  const key = document.createElement('div');
+  const symbol = document.createElement('span');
+  symbol.innerText = text;  
+  key.classList.add(...classList);
+  key.append(symbol); 
+  return key;
+}
+
+function createKeyboard(){
+  const keyboardPannel = document.createElement('div');
+  keyboardPannel.classList.add('keyboard-pannel');
+  const div = document.createElement('div');
+  div.classList.add('keyboard-pannel__keys');
+
+  let key = createButton('h', ['key']);
+  div.append(key);
+  key = createButton('Alt', ['key','key_medium']);
+  div.append(key);
+  key = createButton('Shift', ['key','key_wide']);
+  div.append(key);
+  key = createButton(' ', ['key','key_extra-wide']);
+  div.append(key);
+  key = createButton('Caps Lock', ['key','key_medium', 'key_activatable', 'key_active']);
+  div.append(key);
+
+  keyboardPannel.append(div);
+  return keyboardPannel;
+}
+
+const createDomTree = () => {
+  console.log('domTree');
+  const header = document.createElement('header');
+  const main = document.createElement('main');
+  const footer = document.createElement('footer');
+  const div = document.createElement('div');
+  div.classList.add('wrapper');
+  header.classList.add('header');
+  header.append(div);
+  main.classList.add('main');  
+  footer.classList.add('footer');
+  footer.append(div);
+
+  const screen = createSection('screen');
+  const keyboard = createSection('keyboard');
+  // keyboard.querySelector('.wrapper').append(createKeyboard());
+
+  main.append(screen);
+  main.append(keyboard);
+
+  document.body.append(header);
+  document.body.append(main);
+  document.body.append(footer);
+};
+
+//https://www.youtube.com/watch?v=N3cq0BHDMOY
+
 
 /***/ })
 
 /******/ });
+//# sourceMappingURL=script.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,19 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = (env, options) => {
   const isProduction = options.mode === 'production';
   const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+    watch: !isProduction,
     entry: './src/index.js',
     output: {
       filename: 'script.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.join(__dirname, '/dist'),
     },
 
     plugins : [
       new CleanWebpackPlugin()
     ]
-  };
+  }
   return config;
 };
 
